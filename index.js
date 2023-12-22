73a7678f1fe31c9d15c6cbda375c28e29eaef21e
Tanim1999 <ihtanim56@gmail.com>
2023-12-22 17:24:38 +0600

    added api to remove task

--- index.js
+++ index.js
@@ -88,7 +88,12 @@ async function run() {
             const result = await taskCollection.updateOne(filter, updatedDoc, options)
             res.send(result);
         })
-
+        app.delete('/tasks/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await taskCollection.deleteOne(query);
+            res.send(result);
+        })
 
 
 
